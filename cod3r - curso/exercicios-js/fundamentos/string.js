992fefa6a8582f41d1ec5a5d76098e9a6dbc1e01
Leandro <leaxviana140@gmail.com>
2019-08-08 02:02:51 -0400

    strings

---
+++ cod3r - curso/exercicios-js/fundamentos/string.js
@@ -0,0 +1,17 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4))
+//não vai mostrar erro
+console.log(escola.charAt(5))
+//tabela unicode
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('3'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(1, 3))
+
+console.log('curso '.concat(escola).concat("!"))
+console.log(escola.replace(3, 'e'))
+
+console.log('Ana, Maria, Pedro'.split(', '))
+
