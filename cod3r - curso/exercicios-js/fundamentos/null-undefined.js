77d36fa8508229e24099b08a2cdcb0eb4d10fd7b
Leandro <leaxviana140@gmail.com>
2019-08-08 14:06:01 -0400

    null and undef

---
+++ cod3r - curso/exercicios-js/fundamentos/null-undefined.js
@@ -0,0 +1,14 @@
+let valor
+console.log(valor)
+
+valor = null
+console.log(valor)
+
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.44
+console.log(produto)
+console.log(produto.preco)
