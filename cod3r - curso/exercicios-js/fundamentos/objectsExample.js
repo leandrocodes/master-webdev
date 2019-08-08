152f5f04b00b2db554ceeb8524f391f4e8f15930
Leandro <leaxviana140@gmail.com>
2019-08-08 13:37:06 -0400

    object data types

---
+++ cod3r - curso/exercicios-js/fundamentos/objectsExample.js
@@ -0,0 +1,11 @@
+const prod1 = {}
+prod1.nome = 'Celular top das galaxia'
+prod1.preco = 344.44
+
+console.log(prod1.nome)
+console.log(prod1)
+
+const prod2 = {
+    nome = "camiseta polo",
+    preco = 35.99,
+}
