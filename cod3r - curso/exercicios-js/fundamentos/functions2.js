77df57cf974d894ecc18d11d9e6838acbfbb4d02
Leandro <leaxviana140@gmail.com>
2019-08-14 09:15:17 -0400

    arrow functions

---
+++ cod3r - curso/exercicios-js/fundamentos/functions2.js
@@ -0,0 +1,12 @@
+const imprimirSoma = function(a, b){
+    console.log(a + b)
+}
+imprimirSoma(2, 3)
+//
+const soma = (a,b) =>{
+    return (a + b)
+}
+console.log(soma(4, 4))
+//
+const subtracao = (a,b) => a-b
+console.log(subtracao(4, 1))
