69ffc706003624ef4490c5e99e384a2c19b977aa
Leandro <leaxviana140@gmail.com>
2019-08-14 09:09:33 -0400

    chamada de funções

--- cod3r - curso/exercicios-js/fundamentos/functions.js
+++ cod3r - curso/exercicios-js/fundamentos/functions.js
@@ -1,1 +1,14 @@
-console.log(typeof Object)
+function imprimirSoma(a, b){
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+imprimirSoma(2)
+imprimirSoma(1, 2, 3, 2)
+imprimirSoma()
+
+function soma(a, b=0){
+    return a + b;
+}
+
+console.log(soma(2, 3))
