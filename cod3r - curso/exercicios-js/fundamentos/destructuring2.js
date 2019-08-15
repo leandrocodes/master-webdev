8d8d175707e177027452424d41889fc237587673
Leandro <leaxviana140@gmail.com>
2019-08-15 15:31:08 -0400

    destructuring in arrays

---
+++ cod3r - curso/exercicios-js/fundamentos/destructuring2.js
@@ -0,0 +1,9 @@
+const [a] = [10]
+console.log(a)
+
+const [n1, , n3, n5, n6 = 0] = [10, 7, 9, 8]
+console.log(n1, n3, n5, n6)
+
+
+const [, [, nota]] = [[, 9, 10, 11], [19, 299, 2]]
+console.log(nota)
