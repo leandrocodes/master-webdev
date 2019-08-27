dbb1b2b27bdd9e6843abb18fdc65662f75d6e28f
leandrocodes <leaxviana140@gmail.com>
2019-08-27 16:19:04 -0400

    proto

---
+++ javascript/objetos/herança1.js
@@ -0,0 +1,12 @@
+const ferrari = {
+    modelo: 'f40',
+    velmax: 324
+}
+
+const volvo = {
+    modelo: 'f40',
+    velmax: 324
+}
+
+console.log(ferrari.__proto__)
+console.log(ferrari.__proto__ === Object.prototype)
