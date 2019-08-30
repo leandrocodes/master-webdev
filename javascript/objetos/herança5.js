9537ce2d2f3c498ed5c30962652049a9bdaaebbd
leandrocodes <leaxviana140@gmail.com>
2019-08-29 20:15:33 -0400

    adding methods to array functions

--- javascript/objetos/herança5.js
+++ javascript/objetos/herança5.js
@@ -1,5 +1,9 @@
 String.prototype.reverse = function(){
     return this.split('').reverse().join('')
 }
-
 console.log('Leandro Viana'.reverse())
+
+Array.prototype.first = function() {
+    return this[0]
+}
+console.log([1, 2, 3].first())
