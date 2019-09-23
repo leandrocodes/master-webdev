8f14dcb072c461d2306b9d8285889942f3fcf83d
Leandro <leaxviana140@gmail.com>
2019-09-22 22:49:02 -0400

    pop, push, shift, unshift

---
+++ javascript/arrays/arrayMethods.js
@@ -0,0 +1,13 @@
+const pilotos = ['Vettel', 'LeClerc', 'Raikkonen', 'Stroll']
+
+pilotos.pop()
+console.log(pilotos)
+
+pilotos.push('Verstappen')
+console.log(pilotos)
+
+pilotos.shift()
+console.log(pilotos)
+
+pilotos.unshift('Kvyat')
+console.log(pilotos)
