63ccb0494d674877f75cee00e2b4830e4569393b
Leandro <leaxviana140@gmail.com>
2019-09-22 23:01:59 -0400

    splice and slice

--- javascript/arrays/arrayMethods.js
+++ javascript/arrays/arrayMethods.js
@@ -11,3 +11,15 @@ console.log(pilotos)
 
 pilotos.unshift('Kvyat')
 console.log(pilotos)
+
+pilotos.splice(2, 0, 'Massa', 'Bottas')
+console.log(pilotos)
+
+pilotos.splice(3, 1)
+console.log(pilotos)
+
+const algunsPilotos1 = pilotos.slice(2)
+console.log(algunsPilotos1)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
