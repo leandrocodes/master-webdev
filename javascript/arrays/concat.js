63cd2fd5796e24e03556480af138d8ebc62cd9ec
leandrocodes <leaxviana140@gmail.com>
2019-11-02 23:07:01 -0400

    concat

---
+++ javascript/arrays/concat.js
@@ -0,0 +1,8 @@
+const filhas = ['Ualeskah', 'Cibalena']
+const filhos = ['Uoxiton', 'Uesclei']
+
+let todos = filhas.concat(filhos, 'Fulano')
+console.log(todos)
+
+todos = filhas + filhos
+console.log(todos)
