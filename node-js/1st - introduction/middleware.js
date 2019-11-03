0fdf9685598b9d0ccd2b5318ac321db66637dfa6
leandrocodes <leaxviana140@gmail.com>
2019-11-03 12:06:32 -0400

    ctx example

--- node-js/1st - introduction/middleware.js
+++ node-js/1st - introduction/middleware.js
@@ -8,6 +8,18 @@ const passo2 = (ctx, next) => {
   next()
 }
 
-const passo1 = (ctx) => {
+const passo3 = (ctx) => {
   ctx.valor3 = 'mid3'
 }
+
+const exec = (ctx, ...middlewares) => {
+  const execPasso = indice => {
+    middlewares && indice < middlewares.length &&
+      middlewares[indice](ctx, ()=> execPasso(indice+1))
+  }
+  execPasso(0)
+}
+
+const ctx = {}
+exec(ctx, passo1, passo2, passo3)
+console.log(ctx)
