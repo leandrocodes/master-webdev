d3e8fc7783fca1947672833b15864edcc3fb4383
leandrocodes <leaxviana140@gmail.com>
2019-11-02 21:08:34 -0400

    map

--- javascript/arrays/map1.js
+++ javascript/arrays/map1.js
@@ -1,7 +1,13 @@
 const nums = [1, 2, 3, 4, 5]
 
-let result = nums.map(e =>{
-  return e*2
+let result = nums.map(e => {
+  return e * 2
 })
+console.log(result)
 
+const soma10 = e => e + 10
+const triplo = e => e * 3
+const paraDin = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
+
+result = nums.map(soma10).map(triplo).map(paraDin)
 console.log(result)
