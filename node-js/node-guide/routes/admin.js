ea34930443e38e1803df7adffbedd68cee0d4904
leandrocodes <leaxviana140@gmail.com>
2020-01-11 19:21:47 -0400

    serving html files

--- node-js/node-guide/routes/admin.js
+++ node-js/node-guide/routes/admin.js
@@ -1,8 +1,10 @@
+const path = require('path')
+
 const express = require('express')
 const router = express.Router()
 
 router.get('/add-product', (req, res, next) => {
-  res.send(`<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>`)
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 
 router.post('/add-product', (req, res, next) => {
