cff5a8c956832f895371c388e20c8cd1a647face
leandrocodes <leaxviana140@gmail.com>
2020-01-11 15:32:51 -0400

    navigating between middlewares

--- node-js/node-guide/index.js
+++ node-js/node-guide/index.js
@@ -6,6 +6,11 @@ const app = express()
 
 app.use((req, res, next) => {
   console.log(`In the middleware!`)
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log(`In another middleware!`)
 })
 
 const server = http.createServer(app)
