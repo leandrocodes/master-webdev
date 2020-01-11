06d23d12debf05c2f3e78f7febe25e74ce3f2f1d
leandrocodes <leaxviana140@gmail.com>
2020-01-11 18:25:30 -0400

    restricting for get and post /admin routes

--- node-js/node-guide/index.js
+++ node-js/node-guide/index.js
@@ -11,8 +11,12 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 
 //routes
-app.use(adminRoutes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>404</h1>')
+})
 
 const server = http.createServer(app)
 server.listen(3000)
--- node-js/node-guide/routes/admin.js
+++ node-js/node-guide/routes/admin.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-router.use('/add-product', (req, res, next) => {
-  res.send(`<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>`)
+router.get('/add-product', (req, res, next) => {
+  res.send(`<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>`)
 })
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body)
   res.redirect('/')
 })
