cf2fd231b05d31c3c5fb46c79a5a4550e2bd3ca2
leandrocodes <leaxviana140@gmail.com>
2020-01-11 16:15:17 -0400

    basics about middlewares

--- node-js/node-guide/index.js
+++ node-js/node-guide/index.js
@@ -4,14 +4,14 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log(`In the middleware!`)
-  next()
+app.use('/add-product', (req, res, next) => {
+  console.log(`In add-product middleware!`)
+  res.send(`Sending HTML from add-product middleware!`)
 })
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   console.log(`In another middleware!`)
-  res.send(`Sending!`)
+  res.send(`Sending HTML!`)
 })
 
 const server = http.createServer(app)
