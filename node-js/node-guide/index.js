5742c65e73350d1679d01c6fbafed42e51740018
leandrocodes <leaxviana140@gmail.com>
2020-01-11 15:29:40 -0400

    middleware starting

--- node-js/node-guide/index.js
+++ node-js/node-guide/index.js
@@ -4,5 +4,7 @@ const express = require('express')
 
 const app = express()
 
-const server = http.createServer()
+app.use((req, res, next) => {})
+
+const server = http.createServer(app)
 server.listen(3000)
