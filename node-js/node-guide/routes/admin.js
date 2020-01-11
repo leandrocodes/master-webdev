d0074ef3606c41c80ed9b0bcf352d49b22bddc81
leandrocodes <leaxviana140@gmail.com>
2020-01-11 19:45:39 -0400

    rootDir helper

--- node-js/node-guide/routes/admin.js
+++ node-js/node-guide/routes/admin.js
@@ -1,10 +1,12 @@
 const path = require('path')
-
+//
 const express = require('express')
 const router = express.Router()
+//
+const rootDir = require('../helpers/path')
 
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 router.post('/add-product', (req, res, next) => {
--- node-js/node-guide/routes/shop.js
+++ node-js/node-guide/routes/shop.js
@@ -1,11 +1,13 @@
 const path = require('path')
-
+//
 const express = require('express')
+//
+const rootDir = require('../helpers/path')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 })
 
 module.exports = router
