f11504fe97c71ad0c742d518930fa87e8eee2d47
leandrocodes <leaxviana140@gmail.com>
2020-01-11 21:31:42 -0400

    getting products and iterating

--- node-js/node-guide/routes/shop.js
+++ node-js/node-guide/routes/shop.js
@@ -8,7 +8,8 @@ const adminData = require('./admin')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-  res.render('shop')
+  const products = adminData.products
+  res.render('shop', { prods: products, docTitle: 'My shop' })
   console.log(adminData.products)
 })
 
