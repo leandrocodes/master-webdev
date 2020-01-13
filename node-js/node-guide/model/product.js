e01eba579041c0341eb33690a79677ac4a9ec556
leandrocodes <leaxviana140@gmail.com>
2020-01-13 17:34:06 -0400

    .

--- node-js/node-guide/model/product.js
+++ node-js/node-guide/model/product.js
@@ -5,8 +5,8 @@ const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products
 
 const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, data) => {
-    if (err) { return cb([]) }
-    else { return cb(JSON.parse(data)) }
+    if (err) return cb([])
+    else return cb(JSON.parse(data))
   })
 }
 
