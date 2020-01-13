b39a3faf57548a01a7ccf78103687d70d1d1a0b9
leandrocodes <leaxviana140@gmail.com>
2020-01-13 16:32:20 -0400

    save data in a json file

--- node-js/node-guide/controllers/products.js
+++ node-js/node-guide/controllers/products.js
@@ -11,7 +11,8 @@ exports.postAddProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll()
-  res.render('shop', { prods: products, path: '/' })
-  console.log(products)
+  Product.fetchAll((products) => {
+    res.render('shop', { prods: products, path: '/' })
+    console.log(products)
+  })
 }
--- node-js/node-guide/model/product.js
+++ node-js/node-guide/model/product.js
@@ -3,30 +3,29 @@ const path = require('path')
 
 const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
 
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, data) => {
+    if (err) { return cb([]) }
+    else { return cb(JSON.parse(data)) }
+  })
+}
+
+
 module.exports = class Product {
   constructor(title) {
     this.title = title
   }
 
   save() {
-    fs.readFile(p, (err, data) => {
-      let products = []
-
-      if (!err) products = JSON.parse(data)
-
+    getProductsFromFile(products => {
       products.push(this)
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err)
       })
-
     })
   }
 
-  static fetchAll() {
-    fs.readFile(p, (err, data) => {
-      if (err) return []
-
-      return JSON.parse(data)
-    })
+  static fetchAll(cb) {
+    getProductsFromFile(cb)
   }
 }
