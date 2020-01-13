7e121b276b071eb9d70c1dc960508031be441948
leandrocodes <leaxviana140@gmail.com>
2020-01-13 09:09:29 -0400

    rendering add-product with pug

--- node-js/node-guide/routes/admin.js
+++ node-js/node-guide/routes/admin.js
@@ -10,7 +10,7 @@ const products = []
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+  res.render('add-product') 
 })
 
 // /admin/add-product => POST
