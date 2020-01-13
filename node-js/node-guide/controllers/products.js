a1a92f013500d2f5ab77c7687f40c9b021eaf351
leandrocodes <leaxviana140@gmail.com>
2020-01-13 15:24:25 -0400

    product model

--- node-js/node-guide/controllers/products.js
+++ node-js/node-guide/controllers/products.js
@@ -1,17 +1,17 @@
-const products = []
+const Product = require('../model/product')
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', { path: '/admin/add-product' })
 }
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title })
+  const product = new Product(req.body.title)
+  product.save()
   res.redirect('/')
 }
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll()
   res.render('shop', { prods: products, path: '/' })
   console.log(products)
 }
-
-exports.products = products
