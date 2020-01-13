d6f0c103f1431d5ad78b22ac5a070e3fc65667d3
leandrocodes <leaxviana140@gmail.com>
2020-01-13 16:01:58 -0400

    local json saving

--- node-js/node-guide/model/product.js
+++ node-js/node-guide/model/product.js
@@ -1,4 +1,7 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
 
 module.exports = class Product {
   constructor(title) {
@@ -6,11 +9,24 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this)
+    fs.readFile(p, (err, data) => {
+      let products = []
+
+      if (!err) products = JSON.parse(data)
+
+      products.push(this)
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err)
+      })
+
+    })
   }
 
   static fetchAll() {
-    return products
-  }
+    fs.readFile(p, (err, data) => {
+      if (err) return []
 
+      return JSON.parse(data)
+    })
+  }
 }
