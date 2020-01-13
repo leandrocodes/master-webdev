68d074c246639428d8e1245a9a9cc2f01916480b
leandrocodes <leaxviana140@gmail.com>
2020-01-13 15:09:43 -0400

    error controller

--- node-js/node-guide/index.js
+++ node-js/node-guide/index.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser')
 //
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+//
+const errorController = require('./controllers/error')
 
 const app = express()
 
@@ -19,9 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.use((req, res, next) => {
-  res.status(404).render('404')
-})
+app.use(errorController.get404)
 
 const server = http.createServer(app)
 server.listen(3000)
