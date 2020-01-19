e671369115005c09e044b2fdbd7d4d701632c31c
leandrocodes <leaxviana140@gmail.com>
2020-01-18 23:21:58 -0400

    :construction: cart-item-> cart -> user assoc

--- node-js/node-guide/index.js
+++ node-js/node-guide/index.js
@@ -10,6 +10,7 @@ const sequelize = require('./util/db')
 const Product = require('./models/product')
 const User = require('./models/user')
 const Cart = require('./models/cart')
+const CartItem = require('./models/cart-item')
 
 const app = express()
 
@@ -35,10 +36,12 @@ app.use(shopRoutes)
 app.use(errorController.get404)
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
-User.hasMany(Product)
+User.hasOne(Cart)
+Cart.belongsToMany(Product, { through: CartItem })
+Product.belongsToMany(Cart, { through: CartItem })
 
 sequelize
-  .sync()
+  .sync({force: true})
   .then(result => {
     return User.findByPk(1)
     // console.log(result)
--- node-js/node-guide/models/cart.js
+++ node-js/node-guide/models/cart.js
@@ -1,100 +1,14 @@
-const fs = require('fs')
-const path = require('path')
+const Sequelize = require('sequelize')
 
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'cart.json')
+const sequelize = require('../util/db')
 
-
-//TODO: USAR ESSA FUNÇÃO AQUI!!
-const getCartFromFile = (cb) => {
-  fs.readFile(p, (err, data) => {
-    let cart
-    if (!err) {
-      try {
-        cart = JSON.parse(data)
-      } catch {
-        cart = {}
-      }
-    } else {
-      cart = {}
-    }
-    return cb(cart)
-  })
-}
-
-module.exports = class Cart {
-  static addProduct(id, productPrice) {
-    // Fetch the previous cart
-    this.productPrice = parseFloat(productPrice)
-    fs.readFile(p, (err, data) => {
-      let cart
-      if (!err) {
-        try {
-          cart = JSON.parse(data)
-        } catch {
-          cart = { products: [], totalPrice: 0 }
-        }
-      } else {
-        cart = { products: [], totalPrice: 0 }
-      }
-      // Analyze the cart => Find existing product
-      const existingProductIndex = cart.products.findIndex(prod => prod.id === id)
-      const existingProduct = cart.products[existingProductIndex]
-      let updatedProduct
-      // Add new product / increase quantity
-      if (existingProduct) {
-        updatedProduct = { ...existingProduct }
-        updatedProduct.qty++
-        cart.products = [...cart.products]
-        cart.products[existingProductIndex] = updatedProduct
-      } else {
-        updatedProduct = { id: id, qty: 1 }
-        cart.products = [...cart.products, updatedProduct]
-      }
-      cart.totalPrice += this.productPrice
-      fs.writeFile(p, JSON.stringify(cart), (err) => console.log(err))
-      console.log(cart)
-    })
+const Cart = sequelize.define('cart', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
   }
+})
 
-  static deleteProduct(id, productPrice) {
-    fs.readFile(p, (err, data) => {
-      let cart
-      if (!err) {
-        try {
-          cart = JSON.parse(data)
-        } catch {
-          cart = { products: [], totalPrice: 0 }
-        }
-      } else {
-        cart = { products: [], totalPrice: 0 }
-      }
-      const updatedCart = { ...cart }
-      const product = updatedCart.products.find(prod => prod.id === id)
-      
-      let productQty = 0
-      if (product)
-        productQty = product.qty
-      
-      updatedCart.products = updatedCart.products.filter(prod => prod.id !== id)
-      updatedCart.totalPrice = cart.totalPrice - productPrice * productQty
-      fs.writeFile(p, JSON.stringify(updatedCart), (err) => console.log(err))
-    })
-  }
-
-  static getCart(cb) {
-    fs.readFile(p, (err, data) => {
-      let cart
-      if (!err) {
-        try {
-          cart = JSON.parse(data)
-        } catch {
-          cart = { products: [], totalPrice: 0 }
-        }
-      } else {
-        cart = { products: [], totalPrice: 0 }
-      }
-      cb(cart)
-    })
-  }
-
-}
+module.exports = Cart
